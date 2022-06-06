7f09f3b01f7af3a08cd991b8a22e3496707beada
Mally13 <mallymutuku13@gmail.com>
2022-06-06 14:40:54 +0300

    choice validation

--- RockPaperScissors/main.py
+++ RockPaperScissors/main.py
@@ -8,6 +8,7 @@ while True:
     user_input = input("Enter a choice \n(R  for Rock) \n(P for paper) \n(S for scissors): ")
     player_action=user_input.upper()
 
+ 
     def getfullactionname(s):
         if s=="R":
             return "Rock"
@@ -15,31 +16,35 @@ while True:
             return "Paper"
         elif s=="S":
             return "Scissors"
-          
 
-    print(f"\nPlayer({getfullactionname(player_action)}): CPU({getfullactionname(cpu_action)})")
+    if (player_action in possible_actions):
+        print(f"\nPlayer({getfullactionname(player_action)}): CPU({getfullactionname(cpu_action)})")
 
-    if player_action == cpu_action:
-        print(f"Both CPU and Player selected {getfullactionname(player_action)}. It's a tie, you have to play again!")
+        if player_action == cpu_action:
+            print(f"Both CPU and Player selected {getfullactionname(player_action)}. It's a tie, you have to play again!")
+            play_again="y"
+        elif player_action == "R":
+            if cpu_action == "S":
+                print("Rock smashes scissors! Player wins!")
+            else:
+                print("Paper covers rock! CPU wins.")
+            play_again="n"
+        elif player_action == "P":
+            if cpu_action == "R":
+                print("Paper covers rock! Player wins!")
+            else:
+                print("Scissors cuts paper! CPU wins.")
+            play_again="n"
+        elif player_action == "S":
+            if cpu_action == "P":
+                print("Scissors cuts paper! Player wins!")
+            else:
+                print("Rock smashes scissors! CPU wins.")
+            play_again="n"
+
+    elif(player_action not in possible_actions):
+        print("Invalid choice! You have to play again")
         play_again="y"
-    elif player_action == "R":
-        if cpu_action == "S":
-            print("Rock smashes scissors! Player wins!")
-        else:
-            print("Paper covers rock! CPU wins.")
-        play_again="n"
-    elif player_action == "P":
-        if cpu_action == "R":
-            print("Paper covers rock! Player wins!")
-        else:
-            print("Scissors cuts paper! CPU wins.")
-        play_again="n"
-    elif player_action == "S":
-        if cpu_action == "P":
-            print("Scissors cuts paper! Player wins!")
-        else:
-            print("Rock smashes scissors! CPU wins.")
-        play_again="n"
-
+    
     if play_again.lower() != "y":
      break
